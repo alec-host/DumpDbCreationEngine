d78c8d654a11311b10390ceb15483749ba2e3166
Alex <alex.irungu@inmobia.com>
2023-01-18 10:46:33 +0300

    minor changes in app.conf file, copy_mysql_data.py

--- copy_mysql_data.py
+++ copy_mysql_data.py
@@ -15,10 +15,10 @@ class CopyMySqlData():
         os.system('sudo '+ self.db_service_path +' start')
     #--.copy file.
     def copy_file(self):
-        os.system('sudo ' + self.db_source_path + '/*' + self.db_destination_path)
+        os.system('sudo cp -p ' + self.db_source_path + ' * ' + self.db_destination_path)
     #-..read file meta-data.
     def get_file_metadata(self,file_path):
         return os.stat(file_path)
     #--.delete data folder.
     def delete_command(self):
-        os.system('sudo rm ' + self.db_source_path + '/*')
+        os.system('sudo rm ' + self.db_source_path + '*')
