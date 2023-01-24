bf9450ca52438848766e8471c5793917e62247c6
Alex <alex.irungu@inmobia.com>
2023-01-24 14:49:01 +0300

    add configs params in app settings file

--- configs/app_settings.py
+++ configs/app_settings.py
@@ -14,3 +14,6 @@ source_folder = config.get("file_path","source_folder")
 destination_folder = config.get("file_path","destination_folder")
 compressed_file_source_folder = config.get("file_path","compressed_file_source_folder")
 service_path_command = config.get("service_path","service_path_command")
+
+user = config.get("mysql","db_user")
+password = config.get("mysql","db_password")
